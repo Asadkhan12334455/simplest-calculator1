f5cd34fa0b9ada3aca7b65cc0f43f58db9a4a306
Asadkhan12334455 <160310609+Asadkhan12334455@users.noreply.github.com>
2024-03-31 23:55:08 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your first number", type: "number", name: "firstNumber" },
+    {
+        message: "Enter your secound number",
+        type: "number",
+        name: "secoundNumber",
+    },
+    {
+        message: "Select one of the operators to perfrom operator",
+        type: "list",
+        name: "operator",
+        choices: [
+            "Addition",
+            "Subraction",
+            "Multiplication",
+            "Division",
+            "Modulas",
+            "Exponent",
+            "Equal",
+            "NotEqual",
+            "GreaterThan",
+            "LessThan"
+        ],
+    },
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secoundNumber);
+}
+else if (answer.operator === "Subraction") {
+    console.log(answer.firstNumber - answer.secoundNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secoundNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secoundNumber);
+}
+else if (answer.operator === "Modulas") {
+    console.log(answer.firstNumber % answer.secoundNumber);
+}
+else if (answer.operator === "Exponent") {
+    console.log(answer.firstNumber ** answer.secoundNumber);
+}
+else if (answer.operator === "Equal") {
+    console.log(answer.firstNumber == answer.secoundNumber);
+}
+else if (answer.operator === "NotEqual") {
+    console.log(answer.firstNumber != answer.secoundNumber);
+}
+else if (answer.operator === "GreaterThan") {
+    console.log(answer.firstNumber >= answer.secoundNumber);
+}
+else if (answer.operator === "LessThan") {
+    console.log(answer.firstNumber <= answer.secoundNumber);
+}
